d75d684d6bbf4e1296e73a921bfa2b288cf5e509
florendg <STR01fiets!>
2017-10-09 20:13:49 +0200

    Added comments

--- logger-app/src/main/java/module-info.java
+++ logger-app/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.altran.igjava.logger.app {
 
    requires javafx.controls;
+   //required for javafx
    exports com.altran.igjava.logger.app to javafx.graphics;
 }
--- logger-impl/src/main/java/module-info.java
+++ logger-impl/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.altran.igjava.logger.impl {
    requires log4j.api;
+   //This is how you state a module implements a Service
    provides java.lang.System.LoggerFinder
       with com.altran.igjava.logger.impl.LoggerFinder;
 
